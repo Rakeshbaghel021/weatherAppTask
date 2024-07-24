c450db5147a7cef7207d8f90a30084634dea91d7
Rakesh Baghel <rakesh.b@vetron.in>
2024-07-24 11:35:10 +0530

    fix err msg

--- src/App.js
+++ src/App.js
@@ -277,10 +277,10 @@ function App() {
                                     </dt>
                                     <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
                                       <p className="text-sm font-semibold text-gray-900">
-                                        {data.main.temp_max}&deg; C /{" "}
+                                        {data.main.temp_max}&deg; C{" "}
                                         {data.main.temp_min}&deg; C
                                       </p>
-                                      <p className="text-sm font-semibold text-gray-900">
+                                      <p className="text-sm font-semibold text-green-700">
                                         {"max"}&deg; C {"min"}&deg; C
                                       </p>
                                     </dd>
